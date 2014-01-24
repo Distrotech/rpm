6abfecb9c2749a9692bf5cb3c8a2d82880224f99
Greg Nietsky <gregory@distrotech.co.za>
2014-01-24 20:21:11 +0200

    Include errno.h

--- lib/header.c
+++ lib/header.c
@@ -9,6 +9,7 @@
 /* network byte order and is converted on the fly to host order. */
 
 #include "system.h"
+#include <errno.h>
 #include <netdb.h>
 #include <rpm/rpmtypes.h>
 #include <rpm/rpmstring.h>
